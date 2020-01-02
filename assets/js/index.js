868a210e95f8a4ce2c3d028dbc5140aa0790bfbe
D-Mura <tennis-0819@hotmail.co.jp>
2020-01-02 21:30:25 +0900

    control good btn

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,11 +13,13 @@ $('.del').click(function(){
 
 $(function(){
     var btnSize = $('button').length;
-    if(btnSize > 0){
+    if(btnSize > 1){
         $('.select-val').prop('disabled', false);
         $('.input-val').prop('readonly', false);
+        $('.btn-val').prop('disabled', false);
     }else{
         $('.select-val').prop('disabled', true);
         $('.input-val').prop('readonly', true);
+        $('.btn-val').prop('disabled', true);
     }
 });
